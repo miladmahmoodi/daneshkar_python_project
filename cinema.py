c45b37cc53545735651892c1635a98a144ae8d4f
bardia mirkarimi <bardia.mirkarimi@gmail.com>
2023-05-28 18:51:01 +0330

    cinema class updated

--- cinema.py
+++ cinema.py
@@ -8,26 +8,30 @@ class Cinema:
     movies_list = []
     salon_list = {}
 
-    def __int__(self, name: str, location: str, balance: float):
+    def __int__(self, name: str, location: str):
         self.name = name
         self.location = location
-        self.__balance = balance
+        self.__balance = 0.0
 
     @classmethod
     def add_salon_list(cls, name: str, capacity: int):
-        if not Cinema.is_salon_exist(name):
-            if capacity > 0:
-                cls.salon_list[name] = capacity
-            else:
-                raise exceptions.ZeroCapacityError(messages.Message.CAPACITY)
-        else:
+        if cls.is_salon_exist(name):
             raise exceptions.SameSalonFound(messages.Message.REPEATED_SALON_NAME)
+        if capacity <= 0:
+            raise exceptions.ZeroCapacityError(messages.Message.CAPACITY)
+
+        cls.salon_list[name] = capacity
 
     def save(self):
         pass
 
     @staticmethod
     def get_salon(name: str):
+        """
+        getting salon object from salon dictionary
+        @param name: string
+        @return: salon object
+        """
         if not Cinema.is_salon_exist(name):
             raise exceptions.NoSalonFound(messages.Message.NO_SALON_FOUND)
         else:
@@ -35,4 +39,9 @@ class Cinema:
 
     @staticmethod
     def is_salon_exist(name: str):
+        """
+        check for salon name in salon dictionary
+        @param name: string
+        @return: boolean
+        """
         return name in Cinema.salon_list
