13cffe5788d9175097c9e02ec456e4878cda6d87
bardia mirkarimi <bardia.mirkarimi@gmail.com>
2023-05-29 23:35:11 +0330

    movie class modified except create method

--- movie.py
+++ movie.py
@@ -1,14 +1,16 @@
 import pickle
 from datetime import datetime
-
+from cinema import Cinema
 from utils.exceptions import *
 from utils.messages import Message
+from user import User
+from bank import Bank
 
 
 class Movie:
     movie_list = {}
 
-    def __init__(self, cinema: str, title: str, genre: str, discount: float, age_limit: int,
+    def __init__(self, cinema: str, salon: str, title: str, genre: str, discount: float, age_limit: int,
                  price: float, date_time: datetime = None):
         """
         :param cinema: Cinema name
@@ -23,6 +25,7 @@ class Movie:
         :param total_sales: Total sales
         """
         self.cinema = cinema
+        self.salon = salon
         self.title = title
         self.genre = genre
         self.discount = discount
@@ -40,34 +43,66 @@ class Movie:
         cls.movie_list[title] = new_movie
         return cls.movie_list
 
-    @classmethod
-    def reserve(cls, movie: 'Movie') -> None:
-        """
-        Reserves a movie
-        :param movie: Movie object
-        :return: None
-        """
-        if not movie.is_live:
-            raise ShowDoesNotExist(Message.SHOW_DOES_NOT_EXIST_MESSAGE)
-
-        if datetime.now().date() != movie.date_time.date() or datetime.now().time() != movie.date_time.time():
-            raise ShowDoesNotExist(Message.SHOW_DOES_NOT_EXIST_MESSAGE)
-
-        movie.sold += 1
-        movie.total_sales += 1
-        return save_to_json(movie)
+    # def reserve(self, cinema: "Cinema", user: "User", bank: "Bank") -> None:
+    #     """
+    #
+    #     @param cinema:
+    #     @param user:
+    #     @return:
+    #     """
+    #
+    #     if not self.is_live:
+    #         raise ShowDoesNotExist(Message.SHOW_DOES_NOT_EXIST_MESSAGE)
+    #
+    #     if self.age_limit <= user.age:
+    #         raise AgeLimit(Message.AGE_LIMIT_ERROR)
+    #
+    #     bank.
+    #     user.movies_list.append(self)
+    #     salon_data = cinema.get_salon(self.salon)
+    #     salon_data["capacity"] -= 1
 
     def is_live(self) -> bool:
-        pass
+        if self.date_time <= datetime.now():
+            return True
+
+        return False
 
     @classmethod
     def save(cls):
         with open("database/movie.pickle", "wb") as f:
             pickle.dump(cls.movie_list, f)
-            del cls.movie_list
+        cls.movie_list.clear()
 
     @classmethod
     def load(cls):
         with open("database/movie.pickle", "rb") as f:
             loaded_movie_list = pickle.load(f)
         return loaded_movie_list
+
+    def final_price(self, user: "User"):
+        final_price = self.price
+        if user.birthday.day == self.date_time.day and user.birthday.month == user.birthday.month:
+            final_price -= type(self).apply_discount(self.price, 0.5)
+
+        time_passed = datetime.now() - self.date_time
+        month_passed = int(time_passed.days / 30)
+        if month_passed > 100:
+            month_passed = 100
+
+        final_price -= type(self).apply_discount(self.price, month_passed)
+        return final_price
+
+    @staticmethod
+    def apply_discount(price: float, discount: float = 0.0) -> float:
+        """
+
+        @param price:
+        @param discount:
+        @return:
+        """
+        if not 0 <= discount <= 1:
+            raise DiscountError(Message.DISCOUNT_ERROR)
+
+        discount_price = price * discount
+        return discount_price
